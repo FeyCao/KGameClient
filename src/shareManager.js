9f78edff30b0ee5623982935442b550a855f301f
清山静水心 <caoyongfei030625@qq.com>
2016-09-29 14:34:48 +0800

    优化
    优化

--- src/shareManager.js
+++ src/shareManager.js
@@ -38,10 +38,10 @@ ShareManager.prototype.ConnectServer=function()
 	{
 		gSocketConn.RegisterEvent("onopen",this.ConnectedCallBack);
 		gSocketConn.RegisterEvent("onerror",this.ErrorConnectCallBack);
-		//gSocketConn.Connect('ws://222.66.97.203:5003/');
+		gSocketConn.Connect('ws://222.66.97.203:5003/');
 		//gSocketConn.Connect('ws://180.169.108.231:5003/');
 		//gSocketConn.Connect('ws://192.168.16.250:8484/');//测试
-		gSocketConn.Connect('ws://192.168.16.250:8384/');//调试
+		//gSocketConn.Connect('ws://192.168.16.250:8384/');//调试
 	}
 	else
 	{
